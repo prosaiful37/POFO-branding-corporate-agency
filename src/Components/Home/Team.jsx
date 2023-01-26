85ce363f2f1bdc032a64bf4caf576d7fa1724fb3
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2023-01-26 23:39:04 +0600

    team json data add

--- src/Components/Home/Team.jsx
+++ src/Components/Home/Team.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { FaFacebookF, FaGooglePlusG, FaInstagram, FaTwitter } from "react-icons/fa";
-import team1 from "../../Images/teams/team13.jpg";
-import team2 from "../../Images/teams/team14.jpg";
-import team3 from "../../Images/teams/team15.jpg";
-import team4 from "../../Images/teams/team16.jpg";
-import team5 from "../../Images/teams/team17.jpg";
-import team6 from "../../Images/teams/team18.jpg";
+import { useState } from "react";
+import { useEffect } from "react";
+import {
+  FaFacebookF,
+  FaGooglePlusG,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 import "../Styles/Teams.css";
 
 const Team = () => {
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    fetch("Team.json")
+      .then((res) => res.json())
+      .then((data) => setTeams(data));
+  }, []);
   return (
     <div className="md:w-4/5 mx-auto md:py-[130px]">
       <div className="teams">
@@ -23,222 +30,46 @@ const Team = () => {
           </div>
           <div className="teams-people">
             <div className="grid md:grid-cols-3 gap-5">
-              <div className="">
-                <figure>
-                  <div className="team-image team">
-                    <img className="w-[100%] h-auto block" src={team1} alt="" />
-                    <div className="overlays">
+              {teams.map((team) => (
+                <div className="">
+                  <figure>
+                    <div className="team-image team">
+                      <img
+                        className="w-[100%] h-auto block"
+                        src={team.img}
+                        alt=""
+                      />
+                      <div className="overlays">
                         <div className="icon-social flex justify-center gap-3 font-[14px]">
-                            <a href="#">
-                                <FaFacebookF></FaFacebookF>
-                            </a>
-                            <a href="#">
-                                <FaTwitter></FaTwitter>
-                            </a>
-                            <a href="#">
-                                <FaGooglePlusG></FaGooglePlusG>
-                            </a>
-                            <a href="#">
-                                <FaInstagram></FaInstagram>
-                            </a>
-                            
+                          <a href="#">
+                            <FaFacebookF></FaFacebookF>
+                          </a>
+                          <a href="#">
+                            <FaTwitter></FaTwitter>
+                          </a>
+                          <a href="#">
+                            <FaGooglePlusG></FaGooglePlusG>
+                          </a>
+                          <a href="#">
+                            <FaInstagram></FaInstagram>
+                          </a>
                         </div>
+                      </div>
                     </div>
-                    <div></div>
-                  </div>
-                  
-                  <figcaption>
+
+                    <figcaption>
                       <div className="team-member text-center mt-[20px]">
                         <div className="uppercase text-[12px] font-bold">
-                          Hugh Macleod
+                          {team.name}
                         </div>
                         <div className="uppercase text-[11px] text-[#939393]">
-                          Creative Director
+                          {team.title}
                         </div>
                       </div>
                     </figcaption>
-                </figure>
-              </div>
-              <div className="">
-                <figure>
-                  <div className="team-image team">
-                    <img className="w-[100%] h-auto block" src={team2} alt="" />
-                    <div className="overlays">
-                        <div className="icon-social flex justify-center gap-3 font-[14px]">
-                            <a href="#">
-                                <FaFacebookF></FaFacebookF>
-                            </a>
-                            <a href="#">
-                                <FaTwitter></FaTwitter>
-                            </a>
-                            <a href="#">
-                                <FaGooglePlusG></FaGooglePlusG>
-                            </a>
-                            <a href="#">
-                                <FaInstagram></FaInstagram>
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div></div>
-                  </div>
-                  
-                  <figcaption>
-                      <div className="team-member text-center mt-[20px]">
-                        <div className="uppercase text-[12px] font-bold">
-                        Jhon Macleod
-                        </div>
-                        <div className="uppercase text-[11px] text-[#939393]">
-                          Creative Director
-                        </div>
-                      </div>
-                    </figcaption>
-                </figure>
-              </div>
-              <div className="">
-                <figure>
-                  <div className="team-image team">
-                    <img className="w-[100%] h-auto block" src={team4} alt="" />
-                    <div className="overlays">
-                        <div className="icon-social flex justify-center gap-3 font-[14px]">
-                            <a href="#">
-                                <FaFacebookF></FaFacebookF>
-                            </a>
-                            <a href="#">
-                                <FaTwitter></FaTwitter>
-                            </a>
-                            <a href="#">
-                                <FaGooglePlusG></FaGooglePlusG>
-                            </a>
-                            <a href="#">
-                                <FaInstagram></FaInstagram>
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div></div>
-                  </div>
-                  
-                  <figcaption>
-                      <div className="team-member text-center mt-[20px]">
-                        <div className="uppercase text-[12px] font-bold">
-                        Bill Gardner
-                        </div>
-                        <div className="uppercase text-[11px] text-[#939393]">
-                        Co-Founder / Design
-                        </div>
-                      </div>
-                    </figcaption>
-                </figure>
-              </div>
-              <div className="">
-                <figure>
-                  <div className="team-image team">
-                    <img className="w-[100%] h-auto block" src={team3} alt="" />
-                    <div className="overlays">
-                        <div className="icon-social flex justify-center gap-3 font-[14px]">
-                            <a href="#">
-                                <FaFacebookF></FaFacebookF>
-                            </a>
-                            <a href="#">
-                                <FaTwitter></FaTwitter>
-                            </a>
-                            <a href="#">
-                                <FaGooglePlusG></FaGooglePlusG>
-                            </a>
-                            <a href="#">
-                                <FaInstagram></FaInstagram>
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div></div>
-                  </div>
-                  
-                  <figcaption>
-                      <div className="team-member text-center mt-[20px]">
-                        <div className="uppercase text-[12px] font-bold">
-                        Sara Smith
-                        </div>
-                        <div className="uppercase text-[11px] text-[#939393]">
-                        Creative Studio Head
-                        </div>
-                      </div>
-                    </figcaption>
-                </figure>
-              </div>
-              <div className="">
-                <figure>
-                  <div className="team-image team">
-                    <img className="w-[100%] h-auto block" src={team5} alt="" />
-                    <div className="overlays">
-                        <div className="icon-social flex justify-center gap-3 font-[14px]">
-                            <a href="#">
-                                <FaFacebookF></FaFacebookF>
-                            </a>
-                            <a href="#">
-                                <FaTwitter></FaTwitter>
-                            </a>
-                            <a href="#">
-                                <FaGooglePlusG></FaGooglePlusG>
-                            </a>
-                            <a href="#">
-                                <FaInstagram></FaInstagram>
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div></div>
-                  </div>
-                  
-                  <figcaption>
-                      <div className="team-member text-center mt-[20px]">
-                        <div className="uppercase text-[12px] font-bold">
-                        Joshua Brewer
-                        </div>
-                        <div className="uppercase text-[11px] text-[#939393]">
-                        Creative Studio Head
-                        </div>
-                      </div>
-                    </figcaption>
-                </figure>
-              </div>
-              <div className="">
-                <figure>
-                  <div className="team-image team">
-                    <img className="w-[100%] h-auto block" src={team6} alt="" />
-                    <div className="overlays">
-                        <div className="icon-social flex justify-center gap-3 font-[14px]">
-                            <a href="#">
-                                <FaFacebookF></FaFacebookF>
-                            </a>
-                            <a href="#">
-                                <FaTwitter></FaTwitter>
-                            </a>
-                            <a href="#">
-                                <FaGooglePlusG></FaGooglePlusG>
-                            </a>
-                            <a href="#">
-                                <FaInstagram></FaInstagram>
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div></div>
-                  </div>
-                  
-                  <figcaption>
-                      <div className="team-member text-center mt-[20px]">
-                        <div className="uppercase text-[12px] font-bold">
-                        Jeremy Dupont
-                        </div>
-                        <div className="uppercase text-[11px] text-[#939393]">
-                        Creative Director
-                        </div>
-                      </div>
-                    </figcaption>
-                </figure>
-              </div>
+                  </figure>
+                </div>
+              ))}
             </div>
           </div>
         </div>
